2b3803beaf018750e42cb10ade02fbb4e132a35e
Eugene <oneevgexaone@gmail.com>
2025-07-11 16:19:13 +0500

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+      <main className="flex items-center justify-center min-h-screen bg-gray-50">
+          <h1 className="text-5xl text-purple-600 font-bold">Orlines Shop</h1>
+      </main>
+  )
+}
